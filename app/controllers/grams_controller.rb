8f8ff7c567f83f64f875e8300fc8897e0df97cc2
Rukevwe <rukevwe2006@gmail.com>
2019-08-22 04:23:28 +0000

    secure edit, update & delete

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,9 +4,8 @@ class GramsController < ApplicationController
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.update_attributes(gram_params)
-
     if @gram.valid?
       redirect_to root_path
     else
@@ -17,9 +16,11 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
+
 
   def new
     @gram = Gram.new
@@ -37,6 +38,7 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
 
@@ -56,9 +58,9 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-      render plain: 'Not Found :(', status: :not_found
-    end
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
   end
 
 
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-   describe "grams#destroy action" 
-   it "shouldn't let unauthenticated users destroy a gram" do
+   describe "grams#destroy action" do
+    it "shouldn't allow users who didn't create the gram to destroy it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      delete :destroy, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+
+
+
+
+    it "shouldn't let unauthenticated users destroy a gram" do
       gram = FactoryBot.create(:gram)
       delete :destroy, params: { id: gram.id }
       expect(response).to redirect_to new_user_session_path
@@ -10,6 +22,7 @@ RSpec.describe GramsController, type: :controller do
 
     it "should allow a user to destroy grams" do
       gram = FactoryBot.create(:gram)
+      sign_in gram.user
       delete :destroy, params: { id: gram.id }
       expect(response).to redirect_to root_path
       gram = Gram.find_by_id(gram.id)
@@ -17,6 +30,8 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should return a 404 message if we cannot find a gram with the id that is specified" do
+      user = FactoryBot.create(:user)
+      sign_in user
       delete :destroy, params: { id: 'SPACEDUCK' }
       expect(response).to have_http_status(:not_found)
     end
@@ -25,7 +40,17 @@ RSpec.describe GramsController, type: :controller do
 
 
   describe "grams#update action" do
-     it "shouldn't let unauthenticated users update a gram" do
+    it "shouldn't let users who didn't create the gram update it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      patch :update, params: { id: gram.id, gram: { message: 'wahoo' } }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+
+
+    it "shouldn't let unauthenticated users update a gram" do
       gram = FactoryBot.create(:gram)
       patch :update, params: { id: gram.id, gram: { message: "Hello" } }
       expect(response).to redirect_to new_user_session_path
@@ -33,6 +58,7 @@ RSpec.describe GramsController, type: :controller do
 
     it "should allow users to successfully update grams" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
+      sign_in gram.user
       patch :update, params: { id: gram.id, gram: {message: 'Changed'} }
       expect(response).to redirect_to root_path
       gram.reload
@@ -40,12 +66,16 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should have http 404 error if the gram cannot be found" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
        patch :update, params: { id: "YOLOSWAG", gram: { message: 'Changed'} }
        expect(response).to have_http_status(:not_found)
     end
 
     it "should render the edit form with an http status of unprocessable_entity" do
        gram = FactoryBot.create(:gram, message: "Initial Value")
+       sign_in gram.user
        patch :update, params: { id: gram.id, gram: { message: ''} }
        expect(response).to have_http_status(:unprocessable_entity)
        gram.reload
@@ -55,20 +85,32 @@ RSpec.describe GramsController, type: :controller do
 
 
     describe "grams#edit action" do
-       it "shouldn't let unauthenticated users edit a gram" do
+      it "shouldn't let a user who did not create the gram edit a gram" do
+        gram = FactoryBot.create(:gram)
+        user = FactoryBot.create(:user)
+        sign_in user
+        get :edit, params: { id: gram.id }
+        expect(response).to have_http_status(:forbidden)
+      end
+
+
+     it "shouldn't let unauthenticated users edit a gram" do
       gram = FactoryBot.create(:gram)
       get :edit, params: { id: gram.id }
       expect(response).to redirect_to new_user_session_path
-    end
+     end
 
 
     it "should successfully show the edit form if the gram is found" do
      gram = FactoryBot.create(:gram)
+     sign_in gram.user
      get :edit, params: { id: gram.id}
      expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 error message if the gram is not found" do
+      user = FactoryBot.create(:user)
+      sign_in user
       get :edit, params: { id: 'SWAG' }
       expect(response).to have_http_status(:not_found)
     end
@@ -138,8 +180,9 @@ RSpec.describe GramsController, type: :controller do
        post :create, params: { gram: { message: '' } }
        expect(response).to have_http_status(:unprocessable_entity)
        expect(gram_count).to eq Gram.count
-      end 
-     end
-  end
+     end 
 
-end
+
+    end
+   end
+
